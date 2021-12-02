6948ebf44d15c5dc2d66311ab7f7204534a8f216
Anton Nebera <ne.antonii@gmail.com>
2021-12-02 12:40:46 +0300

    fix

--- persistence.py
+++ persistence.py
@@ -80,7 +80,7 @@ class Persister:
         stales = list(stales)
         if len(stales) == 0:
             return None
-        print(f'[{freq_thresh}] selecting at random a stale word from {stales}')
+        print(f'[{freq_thresh}] selecting at random a stale word from {[s["lemma"] for s in stales]}')
         return random.choice(stales)
 
     def get_new(self, freq_thresh = None):
@@ -91,7 +91,7 @@ class Persister:
         new_ones = list(new_ones)
         if len(new_ones) == 0:
             return None
-        print(f'[{freq_thresh}] selecting at random a new word from {new_ones}')
+        print(f'[{freq_thresh}] selecting at random a new word from {[s["lemma"] for s in stales]}')
         return random.choice(new_ones)
 
     def get_recommended(self):
